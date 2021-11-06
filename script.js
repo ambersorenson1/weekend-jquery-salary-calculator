4f7f8806796156a28eeb3bf4f581d4bf09511f9c
Amber Sorenson <ambersorenson1@gmail.com>
2021-11-05 19:20:52 -0500

    added the renderSalaryOptions function to JS, still working on appending the actual information to the DOM

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-console.log('JS');
-$(document).ready()
-console.log('jq');
+
+$(document).ready(onReady);
+let salaryCalculatorItems = [
+  {fname: Amber,
+    lname: Sorenson,
+    id: 12334,
+    title: Swing-Manager,
+    annualSalary: 90000,
+  }
+];
+
+function onReady(){
+  renderSalaryOptions(salaryCalculatorItems);
+
+}
+
+
+function renderSalaryOptions (itemsToRender){
+  for (let salaryItem of itemsToRender){
+    let newTableRow = ` 
+    <tr>
+        <td>${salaryCalculatorItem.fname}</td>
+        <td>${salaryCalculatorItem.lname}</td>
+        <td>${salaryCalculatorItem.id}</td>
+        <td>${salaryCalculatorItem.title}</td>
+        <td>${salaryCalculatorItem.annualSalary}</td>
+    </tr> 
+    `;
+    $('#salaryCalculatorTableBody').append(newTableRow);
+}
 
